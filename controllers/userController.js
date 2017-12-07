723de0127a8b6ea314fe4e3559445bca948cadcb
Emily Lau <emilylau5@gmail.com>
2017-12-06 16:21:34 -0800

    password encrypt added

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,6 @@
 const db = require("../models");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 module.exports = {
   findAll: function(req, res){
@@ -20,19 +22,31 @@ module.exports = {
         username: dbUser[0].username
       }
       console.log("dbUser.password is", dbUser[0].password)
-    if(dbUser[0].password === req.body.password){
-      res.json(response)
-    }
-    else{
-      res.json(false)
-    }})
-    .catch(err => res.status(422).json(err))
-  },
+      bcrypt.compare(req.body.password, dbUser[0].password, function(error, resp) {
+        if(resp){
+          res.json({
+            auth: true,
+            _id: dbUser[0]._id,
+            username: dbUser[0].username})
+        }
+        else{
+          res.json(false)
+        }
+      })
+    })
+    },
 
   createUser: function(req, res) {
-    db.Users
-    .create(req.body)
-    .then(dbModel => res.json(dbModel))
-    .catch(err => res.status(422).json(err));
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+      db.Users
+      .create({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        username: req.body.username,
+        password: hash,
+        email: req.body.email})
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+    })
   }
 }
